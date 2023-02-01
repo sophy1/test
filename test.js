40feda4959b02783dc624c622264d12869ac450c
sophy1 <jihye.sophy.won@gmail.com>
2023-02-01 14:31:23 +0900

    Update test.js

--- test.js
+++ test.js
@@ -20,3 +20,12 @@ function multiply(a = 1, b) {  // Noncompliant
 }
 
 var x = multiply(42);  // returns NaN as b is undefined
+
+
+function MyClass(name, strings) {
+  name = foo;                    // Noncompliant
+
+  for (var str of strings) {
+    str = "";  // Noncompliant
+  }
+}
